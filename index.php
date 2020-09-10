d4354338973004c2b7533e1e0cfe6da4990323ac
Ryan McCahan <ryan@mccahan.net>
2020-09-10 09:08:33 -0600

    Hero

--- index.php
+++ index.php
@@ -1,23 +1,96 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en-US" class="no-js" prefix="og: http://ogp.me/ns#">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title></title>
-  <link rel="stylesheet" href="style.css">
-  <meta property="og:locale" content="en_US" />
-  <meta property="og:type" content="website" />
-  <meta property="og:title" content="" />
-  <meta property="og:description" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:site_name" content="" />
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:title" content="" />
-</head>
-<body>
 
-  <?php /* The following line is used ONLY for development and can be removed in production */ ?>
-  <?php if (php_sapi_name() == 'cli-server'): ?><script src="//localhost:35729/livereload.js"></script><?php endif; ?>
+<head>
+  <meta charset="UTF-8">
+  <title>Capital COVID-19 Survey</title>
+  <link rel="shortcut icon" href="http://greaterwashingtonpartnership.flywheelsites.com/wp-content/themes/gwp/dist/favicon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+  <link rel='stylesheet' id='screen-css'  href='http://greaterwashingtonpartnership.flywheelsites.com/wp-content/themes/gwp/css/main.min.css?ver=1.0.6' type='text/css' media='all' />
+  <link rel='stylesheet' id='screen-css'  href='style.css' type='text/css' media='all' />
+  <script type='text/javascript' src='http://greaterwashingtonpartnership.flywheelsites.com/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+  <script type='text/javascript' src='http://greaterwashingtonpartnership.flywheelsites.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+</head>
+<body class="home page-template-default page page-id-6 page-homepage">
+	<header role="banner">
+	<a href="#main" class="skip-link">Skip to main content</a>
+	<div class="logo">
+		<a href="/"><img src="http://greaterwashingtonpartnership.flywheelsites.com/wp-content/themes/gwp/dist/logo.svg" width="189" height="63" alt="Greater Washington Partnerships" /></a>
+	</div>
+
+	<button id="menu-btn" class="menu-btn" tabindex="0">
+		<span class="text">Menu</span>
+		<span class="line-1"></span>
+		<span class="line-2"></span>
+	</button>
+</header>
+
+<nav id="nav-main" class="nav-main">
+<h2>Menu</h2>
+<ul id="menu-main-navigation" class="menu"><li id="menu-item-1379" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 current_page_item menu-item-1379"><a href="http://greaterwashingtonpartnership.flywheelsites.com/" aria-current="page">Home</a></li>
+<li id="menu-item-1380" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1380"><a>Who We Are</a>
+<ul class="sub-menu">
+<li id="menu-item-1376" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1376"><a href="http://greaterwashingtonpartnership.flywheelsites.com/our-mission-and-approach/">Our Mission &#038; Approach</a></li>
+<li id="menu-item-1377" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1377"><a href="http://greaterwashingtonpartnership.flywheelsites.com/our-story/">Our Story</a></li>
+<li id="menu-item-1378" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1378"><a href="http://greaterwashingtonpartnership.flywheelsites.com/our-team/">Our Team</a></li>
+</ul>
+</li>
+<li id="menu-item-2195" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2195"><a>Programs &#038; Initiatives</a>
+<ul class="sub-menu">
+<li id="menu-item-1375" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1375"><a href="http://greaterwashingtonpartnership.flywheelsites.com/skills-and-talent/">Skills and Talent</a>
+<ul class="sub-menu">
+<li id="menu-item-1365" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1365"><a href="http://greaterwashingtonpartnership.flywheelsites.com/capital-colab/">Capital CoLAB</a></li>
+</ul>
+</li>
+<li id="menu-item-1373" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1373"><a href="http://greaterwashingtonpartnership.flywheelsites.com/regional-mobility/">Regional Mobility</a>
+<ul class="sub-menu">
+<li id="menu-item-1381" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1381"><a href="/blueprint/">Blueprint for Regional Mobility</a></li>
+</ul>
+</li>
+<li id="menu-item-2196" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2196"><a href="http://greaterwashingtonpartnership.flywheelsites.com/unity-and-solutions/">Unity &#038; Solutions</a></li>
+</ul>
+</li>
+<li id="menu-item-2197" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2197"><a>Thought Leadership</a>
+<ul class="sub-menu">
+<li id="menu-item-1370" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1370"><a href="http://greaterwashingtonpartnership.flywheelsites.com/our-region/">Our Region</a></li>
+<li id="menu-item-1371" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1371"><a href="http://greaterwashingtonpartnership.flywheelsites.com/capital-region-pulse/">Capital Region Pulse</a></li>
+<li id="menu-item-1367" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1367"><a href="http://greaterwashingtonpartnership.flywheelsites.com/events/">Upcoming Events</a></li>
+<li id="menu-item-2147" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2147"><a href="http://greaterwashingtonpartnership.flywheelsites.com/publications/">Publications</a></li>
+</ul>
+</li>
+<li id="menu-item-1382" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1382"><a>Newsroom</a>
+<ul class="sub-menu">
+<li id="menu-item-1372" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1372"><a href="http://greaterwashingtonpartnership.flywheelsites.com/press-releases/">Press Releases</a></li>
+<li id="menu-item-1369" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1369"><a href="http://greaterwashingtonpartnership.flywheelsites.com/media-coverage/">Media Coverage</a></li>
+</ul>
+</li>
+<li id="menu-item-1786" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1786"><a href="http://greaterwashingtonpartnership.flywheelsites.com/contact-us/">Contact Us</a></li>
+</ul>
+  <button id="close-menu" class="close-menu-btn">
+    Close Menu
+    <span class="line"></span>
+    <span class="line"></span>
+  </button>
+</nav>
+
+<section id="hero">
+  <div class="section-content">
+    <div class="left">
+      <h1>Capital COVID-19 Survey:</h1>
+      <h2>Safe Return to Work</h2>
+      <p>Powered by</p>
+      <div class="logos">
+        <img src="dist/logo-gwp.png" srcset="dist/logo-gwp@2x.png 2x" alt="GWP Logo">
+        <img src="dist/logo-ey.png" srcset="dist/logo-ey@2x.png 2x" alt="EY Logo">
+      </div>
+    </div>
+    <div class="right">
+      <img src="dist/header-bg.png" srcset="dist/header-bg@2x.png 2x" alt="">
+    </div>
+  </div>
+</section>
+
+<?php /* The following line is used ONLY for development and can be removed in production */ ?>
+<?php if (php_sapi_name() == 'cli-server'): ?><script src="//localhost:35729/livereload.js"></script><?php endif; ?>
 </body>
 </html>
